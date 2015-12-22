680efd6645faac11f2a72d4d32cc4c772853b1ce
Francisco Marco-Serrano <director@matterof.biz>
2015-12-22 17:18:32 +0000

    Update santa2.R

--- santa2.R
+++ santa2.R
@@ -2,39 +2,34 @@ ptm <- proc.time()
 
 library(data.table)
 
-##Read data
+#Read data
 cat("Reading data... \n")
-gifts <- fread("../input/gifts.csv")
+gifts <- fread("input/gifts.csv")
 
-##Find the number of clusters that satisfy the weight limit
+#Find the number of clusters that satisfy the weight limit
 #Ideally, this should've been done together with the clustering phase, though
 cat("Grouping clusters... \n")
 
-#Initialise TripId
-gifts$TripId <- c(1:100000)
-
-#Set parameters
 set.seed(123)
+k.init <- 5000
 wlimit <- 1000
-k <- 2000
-
 
 Location <- cbind(gifts[, Longitude], gifts[, Latitude])
 summary(Location)
 
 
-for (i in 1000:k)
-{
-    gifts$TripId <- kmeans(Location, centers = k, iter.max = 30)$cluster
-    if (max(aggregate(Weight~TripId, data=gifts, FUN=sum)$Weight) <= wlimit) break
-}
+for (k in 1000:k.init){
+    gifts$TripId <- kmeans(Location, centers = k, iter.max = 50)$cluster    
+    tripWeight <- aggregate(Weight~TripId, data=gifts, FUN=sum)$Weight
+    if (max(tripWeight) <= wlimit) break
+}    
 
-summary(aggregate(Weight~TripId, data=gifts, FUN=sum)$Weight)
+summary(tripWeight)
 
 #Write submission file
 cat("Saving file... \n")
 submit <- data.frame(GiftId=gifts$GiftId, TripId=gifts$TripId)
-write.table(submit, file="submission.csv", row.names = F)
+write.csv(submit, file="submission.csv", row.names = F)
 
 cat("Processing time:")
 proc.time() - ptm
