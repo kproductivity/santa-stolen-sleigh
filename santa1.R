57175fe50eb7ff217f629f58cf26d9cdeec25a05
Francisco Marco-Serrano <director@matterof.biz>
2015-12-22 15:34:55 +0000

    Update santa1.R

--- santa1.R
+++ santa1.R
@@ -2,58 +2,48 @@ ptm <- proc.time()
 
 library(data.table)
 library(geosphere)
-library(fastcluster) #See http://www.jstatsoft.org/article/view/v053i09
-library(ddR)
-library(kmeans.ddR)
 
 #Read data
 cat("Reading data... \n")
 gifts <- fread("input/gifts.csv")
 
-#Calculate first tier clusters
-#as suggested by William Cukierski (TY!)
-Location <- cbind(gifts[, Longitude], gifts[, Latitude])
-summary(Location)
-
-dgifts <- as.darray(Location)
-k <- 5000 #to later tune
-first <- dkmeans(dgifts, centers = k)
-
-#Allocate new centers
-#newCenters <- data.frame(cluster1=c(1:k), first$centers)
-names(newCenters)[2:3]<-c("Longitude", "Latitude")
-firstClusters <- data.frame(GiftId=c(1:100000),
-                            Weight=gifts$Weight,
-                            cluster1=predict(first, Location))
-#firstClusters <- merge(firstClusters, newCenters, by="cluster")
-
-#Calculate distances
-cat("Calculating Haversine distances... \n")
-
-ByLocation <- cbind(first$centers[,1], first$centers[,2])
-d <- as.dist(apply(ByLocation, 1,
-                   function(x) distHaversine(x, ByLocation)))
-
-#Hierarchical cluster analysis
-cat("Calculating clusters... \n")
-hc <- hclust(d, method = "centroid")
-
-
 #Find the number of clusters that satisfy the weight limit
 #Ideally, this should've been done together with the clustering phase, though
 cat("Grouping clusters... \n")
+
+set.seed(123)
+k.init <- 5000
 wlimit <- 1000
 
-for (i in 1:k)
-{
-    newClusters <- data.frame(cluster1=c(1:k), cluster2=cutree(hc, k=i))
-    evalClusters <- merge(firstClusters, newClusters, by="cluster1")
-    if (max(aggregate(Weight~cluster2, data=evalClusters, FUN=sum)$Weight) <= wlimit) break
-}
+Location <- cbind(gifts[, Longitude], gifts[, Latitude])
+summary(Location)
+
+first <- kmeans(Location, centers = k.init, iter.max = 50)    
+gifts$cluster <- first$cluster
+
+#First clustering results
+summary(aggregate(Weight~TripId, data=gifts, sum)$Weight)
+
+
+#Then, hierarchical clustering on the clustered centers
+ByLocation <- cbind(first$centers[,1], first$centers[,2])
+
+d <- as.dist(apply(ByLocation, 1,
+                   function(x) distHaversine(x, ByLocation)))
+
+second <- hclust(d)
+
+for (i in 1000:k.init){
+    newClusters <- data.frame(TripId=c(1:k.init), cluster=cutree(second, k=i))
+    evalClusters <- merge(gifts, newClusters, by="TripId")
+    if (max(aggregate(Weight~cluster, data=evalClusters, FUN=sum)$Weight) <= wlimit) break
+}    
+
 
 #Write submission file
-submit <- data.frame(GiftId=evalClusters$GiftId, TripId=evalClusters$cluster2)
-write.table(submit, file="submission.csv", row.names = F)
+cat("Saving file... \n")
+submit <- data.frame(GiftId=gifts$GiftId, TripId=gifts$cluster)
+write.csv(submit, file="submission.csv", row.names = F)
 
 cat("Processing time:")
 proc.time() - ptm
